818e14fff9b948b6fbb55c47589d5f5bc5388552
s-tamiya <sy.tamiya@gmail.com>
2018-08-06 16:41:56 +0900

    first commit

---
+++ springboot-angular-example/src/main/java/com/mydevgeek/User.java
@@ -0,0 +1,13 @@
+package com.mydevgeek;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+}
---
+++ springboot-angular-example/src/test/java/com/mydevgeek/springbootangularexample/SpringbootAngularExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mydevgeek.springbootangularexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAngularExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
